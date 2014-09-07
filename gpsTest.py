e652cdc51775e73f64674ca50e8e8b33393ca339
Ben Mayo <benmayo36@hotmail.com>
2014-09-07 11:08:32 +0100

    first commit

--- gpsTest.py
+++ gpsTest.py
@@ -4,14 +4,15 @@ import gps
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
+x = 0
 while True:
     try:
         report = session.next()
-        if report['class'] == 'TPV' or 'lat':
+        if report['class'] == 'TPV':
             if hasattr(report, 'time'):
                 print report.time
+                x = x+1
                 
-        
     except KeyError:
         pass
     except KeyboardInterrupt:
