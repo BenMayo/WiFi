30928da5c56f543ffb1216cd0406b6487d0c401f
Ben Mayo <benmayo36@hotmail.com>
2014-09-07 10:48:11 +0100

    first commit

--- gpsTest.py
+++ gpsTest.py
@@ -1,55 +1,24 @@
 import gps
-
+ 
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-
-
-report = session.next()
+ 
+while True:
+    try:
+        report = session.next()
         # Wait for a 'TPV' report and display the current time
         # To see all report data, uncomment the line below
 #         print report
-
-if report['class'] == 'TPV':
-    if hasattr(report, 'time'):
+ 
+        if report['class'] == 'TPV':
+            if hasattr(report, 'time'):
                 print report.time
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import gps
-# 
-# # Listen on port 2947 (gpsd) of localhost
-# session = gps.gps("localhost", "2947")
-# session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-# 
-# while True:
-#     try:
-#         report = session.next()
-#         # Wait for a 'TPV' report and display the current time
-#         # To see all report data, uncomment the line below
-# #         print report
-# 
-#         if report['class'] == 'TPV':
-#             if hasattr(report, 'time'):
-#                 print report.time
-#                 
-#     except KeyError:
-#         pass
-#     except KeyboardInterrupt:
-#         quit()
-#     except StopIteration:
-#         session = None
-#         print "GPSD has terminated"
+                 
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print "GPSD has terminated"
