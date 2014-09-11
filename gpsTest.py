aa6a0f06f22aad9654657e7d3838eca55b55d9eb
Ben Mayo <benmayo36@hotmail.com>
2014-09-11 20:32:58 +0100

    ben and chris commit

--- gpsTest.py
+++ gpsTest.py
@@ -10,17 +10,14 @@ session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
 x = 0
-
 while x<1:
     try:
         report = session.next()
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
-                cursor.execute("insert into train1 values '%s'" % report.time)
                 print report.time
-        session = None
-        print "GPSD has terminated"
-        x = x+1      
+                cursor.execute("insert into train1 values '%s'" % report.time)
+                x = x+1      
     except KeyError:
         pass
     except KeyboardInterrupt:
@@ -28,6 +25,8 @@ while x<1:
     except StopIteration:
         session = None
         print "GPSD has terminated"
+
+
 
 x = 0
 while x<1:
