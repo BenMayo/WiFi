89cee1fe8a7509f4eab01ae8e5767c7ddc5da241
Ben Mayo <benmayo36@hotmail.com>
2014-09-11 19:05:45 +0100

    ben and chris commit

--- sqlLite.py
+++ sqlLite.py
@@ -3,19 +3,10 @@
 import sqlite3
 
 conn = sqlite3.connect('ex.db')
-print "Opened database successfully";
-
-
-
-# 
-
-
-
-# db = sqlite3.connect('/home/pi/Desktop/Wifi/ex')
-# 
-# cursor = db.cursor()
-# cursor.execute('''
-#     CREATE TABLE train1(id INTEGER PRIMARY KEY, lat FLOAT,
-#                        lon FLOAT, timestamp TIMESTAMP, resultText TEXT, resultNum FLOAT)
-# ''')
-# db.commit()
+   
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE train1(id INTEGER PRIMARY KEY, lat FLOAT,
+                       lon FLOAT, timestamp TIMESTAMP, resultText TEXT, resultNum FLOAT)
+''')
+conn.commit()
