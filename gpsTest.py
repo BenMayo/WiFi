9f95de8c3ae31a8b4c7e554840cfef83d1bf596c
Ben Mayo <benmayo36@hotmail.com>
2014-09-11 22:11:39 +0100

    ben and chris commit

--- gpsTest.py
+++ gpsTest.py
@@ -1,17 +1,24 @@
 import gps
+import sqlite3
+
+conn = sqlite3.connect('ex.db')
+cursor = conn.cursor()
+
 
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-while True:
+x = 0
+while x<1:
     try:
         report = session.next()
-        # Wait for a 'TPV' report and display the current time
-        # To see all report data, uncomment the line below
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
                 print report.time
+                timeStamp = report.time
+                cursor.execute("insert into train1 values '%s'" % timeStamp)
+                x = x+1      
     except KeyError:
         pass
     except KeyboardInterrupt:
@@ -19,3 +26,39 @@ while True:
     except StopIteration:
         session = None
         print "GPSD has terminated"
+
+
+
+x = 0
+while x<1:
+    try:
+        report = session.next()
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lon'):
+                print report.lon
+                x = x+1      
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print "GPSD has terminated"
+
+x = 0
+while x<1:
+    try:
+        report = session.next()
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lat'):
+                print report.lat
+                x = x+1      
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print "GPSD has terminated"
+
+
