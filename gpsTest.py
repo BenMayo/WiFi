6d45e42df57973e3d255dbad8051098fd8391c91
Ben Mayo <benmayo36@hotmail.com>
2014-09-11 21:49:02 +0100

    ben and chris commit

--- gpsTest.py
+++ gpsTest.py
@@ -4,46 +4,15 @@ import gps
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-x = 0
-while x<1:
+while True:
     try:
         report = session.next()
+        # Wait for a 'TPV' report and display the current time
+        # To see all report data, uncomment the line below
+        print report
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
                 print report.time
-                x = x+1      
-    except KeyError:
-        pass
-    except KeyboardInterrupt:
-        quit()
-    except StopIteration:
-        session = None
-        print "GPSD has terminated"
-
-x = 0
-while x<1:
-    try:
-        report = session.next()
-        if report['class'] == 'TPV':
-            if hasattr(report, 'lon'):
-                print report.lon
-                x = x+1      
-    except KeyError:
-        pass
-    except KeyboardInterrupt:
-        quit()
-    except StopIteration:
-        session = None
-        print "GPSD has terminated"
-
-x = 0
-while x<1:
-    try:
-        report = session.next()
-        if report['class'] == 'TPV':
-            if hasattr(report, 'lat'):
-                print report.lat
-                x = x+1      
     except KeyError:
         pass
     except KeyboardInterrupt:
