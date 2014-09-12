269296ebbd2421c67de2ab40dba50db0032f2411
Ben Mayo <benmayo36@hotmail.com>
2014-09-12 22:26:12 +0100

    ben

--- locationCheck.py
+++ locationCheck.py
@@ -1,6 +1,14 @@
 # this script will check the current location and decide if a web test is required
 # to start we will do this based just on location, but could also incorporate a time (peak/off peak) element
 
+# the gps posistion and time comes from the gpsTest, so we import that here
 import gpsTest
 
-print gpsTest.lat
+# assign lat lon and timestamp to the variables within gpsTest - this will save us having to reference it throughout the script
+lat = gpsTest.lat
+lon = gpsTest.lon
+timestamp = gpsTest.timeStamp
+
+print lat
+print lon
+print timeStamp
