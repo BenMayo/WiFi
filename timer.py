9c5bdb630a863aae858d502f961ee4e744185d9b
Ben Mayo <benmayo36@hotmail.com>
2014-09-29 13:29:04 +0100

    timer commit

--- timer.py
+++ timer.py
@@ -1,10 +1,20 @@
 # this script is a simple timer, set to run at 5s intervals
 # this will run all of the time, and fire other processes at given intervals
 import seleniumTest
-
 import threading
 
-def printit():
-  threading.Timer(20.0, printit).start()
-execfile ("seleniumTest.py")
-printit()
+
+
+import time
+def executeSomething():
+    while True:
+#         execfile ("seleniumTest.py")
+        print 'hello'
+        time.sleep(10)
+
+# while True:
+#     executeSomething()
+# def printit():
+#     threading.Timer(120.0, printit).start()
+# execfile ("seleniumTest.py")
+# printit()
