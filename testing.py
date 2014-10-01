6b1351247378668d72a6b09f4c15841c31841741
Ben Mayo <benmayo36@hotmail.com>
2014-10-01 19:37:45 +0100

    Commit

--- testing.py
+++ testing.py
@@ -1,3 +1,13 @@
-import seleniumTest
+import gammu
 
-print seleniumTest.__file__
+sm = gammu.StateMachine()
+sm.ReadConfig()
+sm.Init()
+
+message = {
+    'Text': 'python-gammu testing message', 
+    'SMSC': {'Location': 1},
+    'Number': '+420800123465',
+}
+
+sm.SendSMS(message)
